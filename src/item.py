da5256826cc09eed410009362f8eb16eee8774cc
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2023-12-31 00:54:40 +0100

    Update main.py

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+from pathlib import Path
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -8,23 +11,58 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name  # Приватный атрибут name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)  # Добавляем созданный объект в список all
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str) -> None:
+        """
+        Инициализирует экземпляры класса Item данными из CSV-файла.
+        """
+        current_file_path = Path(__file__)
+        file_path = current_file_path.parent.parent / 'src/items.csv'
+
+        with open(file_path, 'r', encoding='windows-1251') as file:
+            cls.all.clear()
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(value: str) -> int:
+        """
+        Преобразует строку в число.
+        """
+        return int(float(value))
+
+Item.instantiate_from_csv('src/items.csv')
+
