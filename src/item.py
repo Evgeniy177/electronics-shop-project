f5eb777ed8a8ddc562eaef27674909304537bb53
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2023-12-31 11:25:03 +0100

    Добавили геттер и сеттер

--- src/item.py
+++ src/item.py
@@ -17,3 +17,26 @@ class Item:
         self.quantity = quantity
         self.all.append(self)  # Добавляем созданный объект в список all
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+        """
+        return self.price * self.quantity
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price *= self.pay_rate
+
