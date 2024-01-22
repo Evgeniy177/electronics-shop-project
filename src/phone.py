d9c1f426c520b1c7681a7ca9d592adfb9378b27c
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2024-01-22 16:04:55 +0100

    phone.py

--- src/phone.py
+++ src/phone.py
@@ -1,20 +1,31 @@
 from src.item import Item
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+    def __init__(self, name: str, price: float, quantity: int, count_of_sim: int):
         super().__init__(name, price, quantity,)
-        self.number_of_sim = number_of_sim
+        self.__count_of_sim = count_of_sim
 
     def __add__(self, other):
-        if isinstance(other, Item):
-            return self.quantity + other.quantity
-        elif isinstance(other, Phone):
+        if isinstance(other, self.__class__) or issubclass(self.__class__, other.__class__):
             return self.quantity + other.quantity
         else:
-            raise TypeError("Нельзя складывать с объектами других классов, кроме Item и Phone")
+            raise ValueError("Складывать можно только экземпляры класса")
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     def __str__(self):
         return f'{self.name}'
+
+    @property
+    def number_of_sim(self):
+        """Геттер sim- карт"""
+        return self.__count_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, num):
+        """Сеттер числа sim- карт"""
+        if float(num) == int(num) and int(num) > 0:
+            self.__count_of_sim = num
+        else:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
